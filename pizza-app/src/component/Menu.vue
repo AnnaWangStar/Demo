b73d42436840dbf69b21a83c7398cca6b91100c5
AnnaWangStar <1875057905@qq.com>
2018-06-26 23:10:48 +0800

    解决购物车重复添加和总价的计算

--- pizza-app/src/component/Menu.vue
+++ pizza-app/src/component/Menu.vue
@@ -11,13 +11,14 @@
                     </tr>
                 </thead>
                 <!-- 将data中的数据展示出来 -->
-                <tbody v-for="item in getMenuItems" :key="item.name">
+                <tbody v-for="(item,index) in getMenuItems" :key="index">
+                    <!-- v-for="(item,index) in baskets" :key="index" -->
                     <tr>
                         <td>
                             <strong>{{item.name}}</strong>
                         </td>
                     </tr>
-                    <tr v-for="option in item.options" :key="option.size">
+                    <tr v-for="(option,index) in item.options" :key="index">
                         <td>{{option.size}}</td>
                         <td>{{option.price}}</td>
                         <td>
@@ -37,10 +38,11 @@
                         <tr>
                             <th>数量</th>
                             <th>品种</th>
-                            <th>价格</th>
+                            <th>单品总价格</th>
                         </tr>
                     </thead>
-                    <tbody v-for="item in baskets" :key="item.name">
+                    <tbody v-for="(item,index) in baskets" :key="index">
+                        <!-- v-for="(item,index) in baskets" :key="index" -->
                         <tr>
                             <td>
                                 <button @click="decreaseQuantity(item)" class="btn btn-sm">-</button>
@@ -52,7 +54,7 @@
                         </tr>
                     </tbody>
                 </table>
-                <p>总价:</p>
+                <p>总价:{{total}} RMB</p>
                 <button class="btn btn-success btn-block">
                     提交
                 </button>
@@ -71,8 +73,8 @@ export default {
     return {
       // 定义一个空数组，来接收传给购物车的值
       baskets: [],
-        // 购物车为空时
-      basketText:"购物车没有任何产品",
+      // 购物车为空时
+      basketText: "购物车没有任何产品",
       getMenuItems: {
         1: {
           name: "榴莲pizza",
@@ -115,33 +117,63 @@ export default {
               price: 46
             }
           ]
-        }
+        },
       }
-    };
+    }
   },
+  //总价的计算
+  computed: {
+    total() {
+      let totalCost = 0;
 
+      for (let index in this.baskets) {
+        let indiviualItem = this.baskets[index];
+        totalCost += indiviualItem.quantity * indiviualItem.price;
+      }
+
+      return totalCost
+    }
+  },
   // 添加到购物车
   methods: {
     addToBasket(item, option) {
-      this.baskets.push({
+      let basket = {
         name: item.name,
         size: option.size,
         price: option.price,
         quantity: 1
-      });
-    },
-    decreaseQuantity(item){
-        item.quantity--
-        if(item.quantity<=0){
-            this.removeFromBasket(item)
+      };
+
+      if (this.baskets.length > 0) {
+        // 过滤-说明已经存在了
+        let result = this.baskets.filter((basket) => {
+            // name和price相同时，就是同一个产品
+          return (basket.name === item.name && basket.price === option.price)
+        })
+
+        if (result != null && result.length > 0) {
+          result[0].quantity++
+        } else {
+          this.baskets.push(basket)
         }
+
+      } else {
+        //  购物车什么都没有
+        this.baskets.push(basket)
+      }
     },
-    increaseQuantity(item){
-         item.quantity++
+    decreaseQuantity(item) {
+      item.quantity--;
+      if (item.quantity <= 0) {
+        this.removeFromBasket(item);
+      }
+    },
+    increaseQuantity(item) {
+      item.quantity++;
     },
     // 处理异常场景：数量减为0时，该商品从购物车里消失
-    removeFromBasket(item){
-        this.baskets.splice(this.baskets.indexOf(item),1)
+    removeFromBasket(item) {
+      this.baskets.splice(this.baskets.indexOf(item), 1);
     }
   },
 
