20cf2f755cfcab47024640da919676fe57bd4b4d
AnnaWangStar <1875057905@qq.com>
2018-07-17 08:09:16 +0800

    斐波那契数列，递归

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
     <div class="container">
-      <calculator></calculator>
-      <demo1></demo1>
+      <!-- <calculator></calculator> -->
+      <!-- <demo1></demo1>
       <demo2></demo2>
       <demo3></demo3>
-      <demo4></demo4>
-
+      <demo4></demo4> -->
+      <demo5></demo5>
     </div>
   </div>
 </template>
@@ -17,7 +17,7 @@ import Demo1 from "./component/Demo1";
 import Demo2 from "./component/Demo2";
 import Demo3 from "./component/Demo3";
 import Demo4 from "./component/Demo4";
-
+import Demo5 from "./component/Demo5";
 
 export default {
   components: {
@@ -26,8 +26,7 @@ export default {
     demo2: Demo2,
     demo3: Demo3,
     demo4: Demo4,
-
-
+    demo5: Demo5
   }
 };
 </script>
